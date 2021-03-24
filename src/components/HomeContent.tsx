3f489921bc95cb980b65f25b1d947bd57b0192db
moyongkexing <moyongkexing@gmail.com>
2021-03-24 20:45:57 +0900

    💄 update styles

--- src/components/HomeContent.tsx
+++ src/components/HomeContent.tsx
@@ -13,15 +13,14 @@ const HomeContent: VFC<Props> = (props) => {
   const { linkProps, imgProps, name } = props;
 
   return (
+    // hoverすると上に移動
     <Link {...linkProps}>
-      <div className="w-44 h-44 bg-blue-200 flex justify-center items-center m-5 cursor-pointer">
-        <a>
-          <div>
-            <Image {...imgProps} alt="コンテンツの画像" />
-          </div>
-          {/* <span>{name}</span> */}
-        </a>
-      </div>
+      <a className="w-44 h-52 xl:h-44 bg-blue-200  m-5 cursor-pointer border-2 border-gray-300 text-center overflow-hidden">
+        <div className="flex justify-center items-center my-4 xl:transition-all xl:duration-500 xl:ease-in-out xl:mt-0 xl:h-full xl:hover:-mt-36 ">
+          <Image className="" {...imgProps} alt="コンテンツの画像" />
+        </div>
+        <span className="">{name}</span>
+      </a>
     </Link>
   );
 };
