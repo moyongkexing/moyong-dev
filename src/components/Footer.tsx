4e6ea09a611155740ebff86892f50e87af0f0d86
moyongkexing <moyongkexing@gmail.com>
2021-03-30 14:35:42 +0900

    💄 update responsive

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -3,22 +3,22 @@ import Link from "next/link";
 
 const Footer: VFC = () => {
   return (
-    <footer className="flex flex-col items-center text-sm p-4">
-      <ul className="flex flex-col items-center w-full justify-between sm:flex-row">
-        <li className="mt-4 sm:mt-0 sm:ml-4">
+    <footer className="w-full border-t border-gray-300 flex flex-col items-center text-sm p-4">
+      <ul className="flex flex-col items-center w-full justify-between md:w-1/2 md:flex-row lg:w-1/3">
+        <li className="mt-4 md:mt-0 sm:ml-4">
           <Link href="/">ホーム</Link>
         </li>
-        <li className="mt-4 sm:mt-0 sm:ml-4">
-          <Link href="/profile">運営者</Link>
+        <li className="mt-4 md:mt-0 sm:ml-4">
+          <Link href="/profile">プロフィール</Link>
         </li>
-        <li className="mt-4 sm:mt-0 sm:ml-4">
+        <li className="mt-4 md:mt-0 sm:ml-4">
           <Link href="/works">作品</Link>
         </li>
-        <li className="mt-4 sm:mt-0 sm:ml-4">
+        <li className="mt-4 md:mt-0 sm:ml-4">
           <Link href="/contact">お問い合わせ</Link>
         </li>
       </ul>
-      <p className="uppercase mt-8">&copy; 2021 MO4g-DEV</p>
+      <p className="mt-8">&copy; 2021 MO4g-DEV</p>
     </footer>
   );
 };
