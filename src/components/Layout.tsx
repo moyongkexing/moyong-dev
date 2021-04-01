b35764ed635899e736105ddd38966f79afbee77c
moyongkexing <moyongkexing@gmail.com>
2021-04-01 14:59:38 +0900

    💄 add margin to main component

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -1,4 +1,4 @@
-import type { ReactNode, VFC } from "react";
+import React, { ReactNode, VFC } from "react";
 import Footer from "src/components/Footer";
 import Header from "src/components/Header";
 
@@ -6,7 +6,7 @@ const Layout: VFC<{ children: ReactNode }> = (props) => {
   return (
     <div className="bg-white w-full flex flex-col justify-center items-center shadow-xl border-2 border-gray-300 md:w-11/12 xl:w-10/12">
       <Header />
-      <main className="">{props.children}</main>
+      <main className="my-10">{props.children}</main>
       <Footer />
     </div>
   );
